374fcfe32f227e507f570429a00262828af17775
Epimodev <godin.fred8@gmail.com>
2021-10-18 10:46:00 +0200

    rename uniforms with u_ prefix

--- src/fire/index.ts
+++ src/fire/index.ts
@@ -3,21 +3,21 @@ import type { SceneAssets } from "../utils/assets"
 import { fireFragment, fireVertex } from "./shaders.glslx"
 
 export type FireUniforms = {
-  bendScale: THREE.IUniform<number>
-  bendOrigin: THREE.IUniform<THREE.Vector2>
-  verticalBend: THREE.IUniform<number>
-  horizontalBend: THREE.IUniform<number>
-  trailPattern: THREE.IUniform<THREE.Texture>
-  trailMask: THREE.IUniform<THREE.Texture>
-  trailNoise: THREE.IUniform<THREE.Texture>
-  patternScale: THREE.IUniform<number>
-  patternOffset: THREE.IUniform<number>
-  patternDeform: THREE.IUniform<number>
-  maskOffset: THREE.IUniform<number>
-  color1: THREE.IUniform<THREE.Color>
-  color2: THREE.IUniform<THREE.Color>
-  color3: THREE.IUniform<THREE.Color>
-  color4: THREE.IUniform<THREE.Color>
+  u_bendScale: THREE.IUniform<number>
+  u_bendOrigin: THREE.IUniform<THREE.Vector2>
+  u_verticalBend: THREE.IUniform<number>
+  u_horizontalBend: THREE.IUniform<number>
+  u_trailPattern: THREE.IUniform<THREE.Texture>
+  u_trailMask: THREE.IUniform<THREE.Texture>
+  u_trailNoise: THREE.IUniform<THREE.Texture>
+  u_patternScale: THREE.IUniform<number>
+  u_patternOffset: THREE.IUniform<number>
+  u_patternDeform: THREE.IUniform<number>
+  u_maskOffset: THREE.IUniform<number>
+  u_color1: THREE.IUniform<THREE.Color>
+  u_color2: THREE.IUniform<THREE.Color>
+  u_color3: THREE.IUniform<THREE.Color>
+  u_color4: THREE.IUniform<THREE.Color>
 }
 
 export const createFire = ({
@@ -28,21 +28,21 @@ export const createFire = ({
   const fireSize = 0.75
   const fireDivisions = 10
   const fireUniforms: FireUniforms = {
-    bendScale: { value: 0.5 },
-    bendOrigin: { value: new THREE.Vector2(-fireSize / 2, 0) },
-    verticalBend: { value: 0 },
-    horizontalBend: { value: 0 },
-    trailPattern: { value: swordTrail },
-    trailMask: { value: swordTrailMask },
-    trailNoise: { value: swordTrailNoise },
-    patternScale: { value: 2 },
-    patternOffset: { value: 0 },
-    patternDeform: { value: 0.133 },
-    maskOffset: { value: 0 },
-    color1: { value: new THREE.Color(0xe74c3c) },
-    color2: { value: new THREE.Color(0xe67e22) },
-    color3: { value: new THREE.Color(0xf1c40f) },
-    color4: { value: new THREE.Color(0xffffff) },
+    u_bendScale: { value: 0.5 },
+    u_bendOrigin: { value: new THREE.Vector2(-fireSize / 2, 0) },
+    u_verticalBend: { value: 0 },
+    u_horizontalBend: { value: 0 },
+    u_trailPattern: { value: swordTrail },
+    u_trailMask: { value: swordTrailMask },
+    u_trailNoise: { value: swordTrailNoise },
+    u_patternScale: { value: 2 },
+    u_patternOffset: { value: 0 },
+    u_patternDeform: { value: 0.133 },
+    u_maskOffset: { value: 0 },
+    u_color1: { value: new THREE.Color(0xe74c3c) },
+    u_color2: { value: new THREE.Color(0xe67e22) },
+    u_color3: { value: new THREE.Color(0xf1c40f) },
+    u_color4: { value: new THREE.Color(0xffffff) },
   }
   const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(fireSize, fireSize, fireDivisions),
--- src/fire/tweakpane.ts
+++ src/fire/tweakpane.ts
@@ -4,55 +4,55 @@ import type { FireUniforms } from "./index"
 export const createFirePane = (uniforms: FireUniforms): void => {
   const pane = new Tweakpane.Pane()
 
-  pane.addInput(uniforms.patternScale, "value", {
+  pane.addInput(uniforms.u_patternScale, "value", {
     label: "Pattern scale",
     min: 0.5,
     max: 3,
     step: 0.01,
   })
-  pane.addInput(uniforms.patternOffset, "value", {
+  pane.addInput(uniforms.u_patternOffset, "value", {
     label: "Pattern offset",
     min: 0,
     max: 100,
     step: 0.01,
   })
-  pane.addInput(uniforms.patternDeform, "value", {
+  pane.addInput(uniforms.u_patternDeform, "value", {
     label: "Pattern deform",
     min: 0,
     max: 0.25,
     step: 0.001,
   })
-  pane.addInput(uniforms.maskOffset, "value", {
+  pane.addInput(uniforms.u_maskOffset, "value", {
     label: "Mask offset",
     min: 0,
     max: 1,
     step: 0.01,
   })
-  pane.addInput(uniforms.bendScale, "value", {
+  pane.addInput(uniforms.u_bendScale, "value", {
     label: "Bend scale",
     min: 0,
     max: 1,
     step: 0.01,
   })
-  pane.addInput(uniforms.bendOrigin.value, "y", {
+  pane.addInput(uniforms.u_bendOrigin.value, "y", {
     label: "Bend origin",
     min: -1,
     max: 1,
     step: 0.01,
   })
-  pane.addInput(uniforms.verticalBend, "value", {
+  pane.addInput(uniforms.u_verticalBend, "value", {
     label: "Vertical bend",
     min: -Math.PI,
     max: Math.PI,
     step: 0.01,
   })
-  pane.addInput(uniforms.bendOrigin.value, "x", {
+  pane.addInput(uniforms.u_bendOrigin.value, "x", {
     label: "Horizontal bend origin",
     min: -1,
     max: 1,
     step: 0.01,
   })
-  pane.addInput(uniforms.horizontalBend, "value", {
+  pane.addInput(uniforms.u_horizontalBend, "value", {
     label: "Horizontal bend",
     min: -Math.PI,
     max: Math.PI,
